fd2859d7026fb3908a85fe47cfd164afa6409349
paradox8599 <paradox8599@outlook.com>
2023-08-13 23:08:19 +1000

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,175 @@
-fn main() {
-    
+use regex::Regex;
+use serde_json::{Error, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let room_id = read_room_id();
+    let quality = read_quality();
+    let format = read_format();
+
+    println!("正在获取直播源...\n");
+    let stream = fetch_stream(room_id, quality).await?;
+    let urls = parse_stream(stream);
+
+    let urls = urls
+        .iter()
+        .filter(|u| u.contains(&format.value()))
+        .collect::<Vec<&String>>();
+
+    for url in urls.iter() {
+        println!("{}\n", url.trim_matches('"'));
+    }
+
+    Ok(())
+}
+
+fn read_room_id() -> u32 {
+    let mut line = String::new();
+    let mut room_id: Result<u32, &'static str> = Err("init");
+    while room_id.is_err() {
+        println!("输入房间号或直播间地址: ");
+        std::io::stdin().read_line(&mut line).unwrap();
+
+        let re = Regex::new(r"(http[s]?://)?(live.bilibili.com/)?(\d+)").unwrap();
+        let caps = re.captures(line.trim());
+
+        room_id = match caps {
+            None => Err("直播间地址或房间号格式不正确。"),
+            Some(caps) => Ok(caps.get(3).unwrap().as_str().parse::<u32>().unwrap()),
+        };
+        if let Err(e) = room_id {
+            println!("{}", e);
+        }
+    }
+    room_id.unwrap()
+}
+
+fn read_quality() -> Quality {
+    let mut line = String::new();
+    let mut result: Result<Quality, &'static str> = Err("init");
+
+    while result.is_err() {
+        println!("\n选择画质:");
+        println!("1. 流畅");
+        println!("2. 原画\n");
+
+        std::io::stdin().read_line(&mut line).unwrap();
+        result = match line.trim().parse::<u32>() {
+            Ok(1) => Ok(Quality::Low),
+            Ok(2) => Ok(Quality::High),
+            _ => Err(""),
+        };
+    }
+    result.unwrap()
+}
+
+fn read_format() -> Format {
+    let mut line = String::new();
+    let mut result: Result<Format, &'static str> = Err("init");
+
+    while result.is_err() {
+        println!("\n选择格式:");
+        println!("1. m3u8");
+        println!("2. flv\n");
+
+        std::io::stdin().read_line(&mut line).unwrap();
+        result = match line.trim().parse::<u32>() {
+            Ok(1) => Ok(Format::M3u8),
+            Ok(2) => Ok(Format::Flv),
+            _ => Err(""),
+        };
+    }
+    result.unwrap()
+}
+
+enum Format {
+    M3u8,
+    Flv,
+}
+impl Format {
+    fn value(&self) -> String {
+        match self {
+            Self::M3u8 => "m3u8".to_string(),
+            Self::Flv => "flv".to_string(),
+        }
+    }
+}
+
+#[allow(dead_code)]
+enum Quality {
+    Low,
+    High,
+}
+impl Quality {
+    fn value(&self) -> u32 {
+        match self {
+            Self::Low => 0,
+            Self::High => 10000,
+        }
+    }
+}
+
+async fn fetch_stream(room_id: u32, qn: Quality) -> Result<Vec<Value>, &'static str> {
+    let base_url = "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo";
+    let url = format!(
+        "{}?qn={}&protocol=0,1&format=0,1,2&codec=0,1&room_id={}",
+        base_url,
+        qn.value(),
+        room_id
+    );
+    let resp = reqwest::get(url).await;
+    if resp.is_err() {
+        return Err("网络请求出错，请稍后再试。");
+    }
+    let resp = resp.unwrap();
+    let resp = resp.text().await.unwrap();
+    let resp: Result<Value, Error> = serde_json::from_str(&resp);
+    if resp.is_err() {
+        return Err("接口返回格式错误");
+    }
+    let resp = resp.unwrap();
+
+    let code = &resp["code"].as_i64().unwrap();
+    if *code != 0 {
+        return Err("请求出错。");
+    }
+
+    let live_status = &resp["data"]["live_status"];
+    if live_status.as_i64().unwrap() == 0 {
+        return Err("未开播。");
+    }
+
+    let stream = &resp["data"]["playurl_info"]["playurl"]["stream"]
+        .as_array()
+        .unwrap();
+
+    Ok(stream.to_owned().to_owned())
+}
+
+fn parse_stream(stream: Vec<Value>) -> Vec<String> {
+    stream
+        .iter()
+        .flat_map(|s| {
+            s["format"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .flat_map(|f| {
+                    f["codec"]
+                        .as_array()
+                        .unwrap()
+                        .iter()
+                        .flat_map(|c| {
+                            c["url_info"]
+                                .as_array()
+                                .unwrap()
+                                .iter()
+                                .map(|i| format!("{}{}{}", i["host"], c["base_url"], i["extra"]))
+                                .collect::<Vec<String>>()
+                        })
+                        .collect::<Vec<String>>()
+                })
+                .collect::<Vec<String>>()
+        })
+        .collect::<Vec<String>>()
 }
